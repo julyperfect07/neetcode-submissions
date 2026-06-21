430f389984b5276d711f5f7128362d0423a19a33
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-06-21 16:22:26 +0300

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let count = 0
+
+        function isPalindrome(l , r ) {
+                let found = 0
+                while (l >= 0 && r < s.length && s[l] == s[r]) {
+                    found++
+                    l--
+                    r++
+                }
+            return found
+        }
+
+        for(let i = 0 ; i < s.length ; i++) {
+            let l = i 
+            let r = i
+            count += isPalindrome(l , r)
+
+            count += isPalindrome(l , r+ 1)
+            
+        }
+
+        return count
+
+    }
+}
