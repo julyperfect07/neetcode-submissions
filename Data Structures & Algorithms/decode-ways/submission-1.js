b59756a3a50cedbfd51d91f57dcd4d33e503320b
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-06-22 15:44:36 +0300

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    numDecodings(s) {
+        const dp = new Map()
+        dp.set(s.length , 1)
+
+        function dfs(i) {
+            if(dp.has(i)) {
+                return dp.get(i)
+            }
+            if(s.charAt(i) == "0") {
+                return 0
+            }
+            let res = dfs(i+1)
+            if(i + 1 < s.length && (s.charAt(i) == "1" || s.charAt(i) == "2" && s.charAt(i+1) < "7") ) {
+                res += dfs(i+2)
+            }
+            dp.set(i , res)
+            return res
+        }
+
+        return dfs(0)
+
+    }
+}
