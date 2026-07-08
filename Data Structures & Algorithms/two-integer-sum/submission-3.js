bba437b8548afaed77a30ddf83dbb430e2d93850
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-07-08 14:59:02 +0300

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = {}
+
+        for(let i = 0 ; i < nums.length ; i++) {
+            let num = nums[i]
+            let wantedNum = target - num
+            if(wantedNum in map) return [map[wantedNum] , i]
+            map[num] = i
+        }
+
+        return []
+    }
+}
