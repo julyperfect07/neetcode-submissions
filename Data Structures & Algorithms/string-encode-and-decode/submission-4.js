580ca93de3ffffdc2dbb8250ef623bf032205976
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-07-09 14:59:44 +0300

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = ""
+        for(let i of strs ) {
+            res += i.length + "#" + i
+        }
+
+        return res
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+
+        let res = []
+        let i = 0
+        while (i < str.length) {
+            let j = i 
+            while(str[j] !== "#") {
+                j++
+            }
+            let length = parseInt(str.slice(i , j))
+            j++
+
+            res.push(str.slice(j , j + length ))
+            i = j + length 
+        }
+
+        return res
+    }
+}
