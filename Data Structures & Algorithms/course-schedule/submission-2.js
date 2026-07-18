09dc5b92ac97d7dd4c03243cd38438075bed8acb
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-07-18 14:16:57 +0300

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let preMap = Array.from({length : numCourses} , () => [])
+
+        for(let [crs , pre] of prerequisites) {
+            preMap[crs].push(pre)
+        }
+        let visited = new Set()
+        function dfs (crs) {
+            if(visited.has(crs)) return false 
+            if(preMap[crs].length === 0 ) {
+                return true
+            }
+
+            visited.add(crs)
+            for(let pre of preMap[crs]) {
+                if(!dfs(pre)) return false 
+            }
+            visited.delete(crs)
+            preMap[crs] = []
+
+            return true 
+        }
+        for(let i = 0 ; i < numCourses ; i++) {
+            if(!dfs(i)) return false
+        }
+        return true 
+    }
+
+}
