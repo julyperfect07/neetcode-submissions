0f420907967dae85ab66bfc5d0e44a863556cbe0
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-07-21 14:17:07 +0300

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(matrix) {
+        let l = 0 
+        let r = matrix.length - 1
+        while(l < r) {
+            for(let i = 0 ; i < r -  l ; i++) {
+                let top = l
+                let bottom = r
+                let topLeft = matrix[top][l + i]
+                matrix[top][l + i] = matrix[bottom - i][l]
+                matrix[bottom - i ][l] = matrix[bottom][r - i]
+                matrix[bottom][r - i] = matrix[top + i][r]
+                matrix[top + i][r] = topLeft
+            }
+            r--
+            l++
+        }
+    }
+}
