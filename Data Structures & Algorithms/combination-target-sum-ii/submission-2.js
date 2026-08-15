067614090c7f4942d182a95d51a4a2ba317160a0
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-08-15 12:57:02 +0300

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates, target) {
+        let res = []
+        candidates.sort((a,b) => a -b)
+        function dfs (i ,  curr , total ) {
+            if(total == target) {
+                res.push([...curr])
+                return
+            }
+            if(total > target || i == candidates.length) {
+                return
+            }
+
+            curr.push(candidates[i])
+            dfs(i + 1 , curr , total + candidates[i])
+            curr.pop()
+
+            while(i + 1 < candidates.length && candidates[i] == candidates[i+1]){
+                i++
+            }
+
+            dfs(i + 1 , curr , total)
+        }
+        dfs(0 , [] , 0)
+
+        return res
+    }
+}
