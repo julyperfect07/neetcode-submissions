f0419b2807d20971426eec06be513cd2992f141f
Abdallah Yousef <142270354+julyperfect07@users.noreply.github.com>
2026-08-22 22:40:06 +0300

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        if(nums.length === 0 ) {
+            return [[]]
+        }
+
+        let perms = this.permute(nums.slice(1))
+        let res = []
+        for(let p of perms) {
+            for(let i = 0 ; i <= p.length ; i++ ) {
+                let pcopy = [...p]
+                pcopy.splice(i, 0 , nums[0])
+                res.push(pcopy)
+            } 
+        }
+        return res
+    }
+}
